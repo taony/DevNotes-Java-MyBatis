cc6b5ad336ff04f1279e750164c0ee2002453b48
taony <taonyzhang@gmail.com>
2016-07-27 19:23:15 +0800

    add

--- com-devnotes-mybatis/src/main/java/com/taony/mybatis/entity/SysUserInfo.java
+++ com-devnotes-mybatis/src/main/java/com/taony/mybatis/entity/SysUserInfo.java
@@ -1,26 +1,35 @@
 package com.taony.mybatis.entity;
 
 public class SysUserInfo {
-	public String getUserid() {
-		return userid;
-	}
-	public void setUserid(String userid) {
-		this.userid = userid;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	
 	private String userid;
 	private String username;
 	private String email;
 	
+	public String getUserid() {
+		return userid;
+	}
+
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+
 }
